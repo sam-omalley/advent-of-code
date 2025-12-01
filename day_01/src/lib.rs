6ca1adecd3992e89d24cfad05bdf06be0d676bfb
Samuel O'Malley <git@sam.omalley.cc>
2025-12-01 21:41:02 +1030

    Move modulo to the zero check, rather than individual dials

--- day_01/src/lib.rs
+++ day_01/src/lib.rs
@@ -39,8 +39,7 @@ impl Move {
             Move::Right(clicks) => clicks,
         };
 
-        // Rust modulo keeps the sign of the input, so we need to use mathematical modulo.
-        (position + delta).rem_euclid(100)
+        position + delta
     }
 }
 
@@ -59,7 +58,8 @@ pub fn get_password(input: &str) -> i32 {
 
         position = movement.apply(position);
 
-        if position == 0 {
+        // Rust modulo keeps the sign of the input, so we need to use mathematical modulo.
+        if position.rem_euclid(100) == 0 {
             password += 1;
         }
     }
@@ -73,16 +73,16 @@ mod tests {
 
     #[test]
     fn test_dial() {
-        assert_eq!(82, Move::Left(68).apply(50));
-        assert_eq!(52, Move::Left(30).apply(82));
-        assert_eq!(0, Move::Right(48).apply(52));
-        assert_eq!(95, Move::Left(5).apply(0));
-        assert_eq!(55, Move::Right(60).apply(95));
-        assert_eq!(0, Move::Left(55).apply(55));
-        assert_eq!(99, Move::Left(1).apply(0));
-        assert_eq!(0, Move::Left(99).apply(99));
-        assert_eq!(14, Move::Right(14).apply(0));
-        assert_eq!(32, Move::Left(82).apply(14));
+        assert_eq!(82, Move::Left(68).apply(50).rem_euclid(100));
+        assert_eq!(52, Move::Left(30).apply(82).rem_euclid(100));
+        assert_eq!(0, Move::Right(48).apply(52).rem_euclid(100));
+        assert_eq!(95, Move::Left(5).apply(0).rem_euclid(100));
+        assert_eq!(55, Move::Right(60).apply(95).rem_euclid(100));
+        assert_eq!(0, Move::Left(55).apply(55).rem_euclid(100));
+        assert_eq!(99, Move::Left(1).apply(0).rem_euclid(100));
+        assert_eq!(0, Move::Left(99).apply(99).rem_euclid(100));
+        assert_eq!(14, Move::Right(14).apply(0).rem_euclid(100));
+        assert_eq!(32, Move::Left(82).apply(14).rem_euclid(100));
     }
 
     #[test]
