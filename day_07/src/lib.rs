1c8f207f88148f9b9b4519307687dd3a5a1f46cd
Samuel O'Malley <git@sam.omalley.cc>
2025-12-07 18:30:54 +1030

    Complete day 7 part 2

--- day_07/src/lib.rs
+++ day_07/src/lib.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::collections::HashSet;
 
 #[derive(Default)]
@@ -27,7 +28,6 @@ impl Manifold {
 
     pub fn get_num_splits(&self) -> usize {
         let (start_row, start_col) = self.start.expect("Start _must_ be set");
-        //self.get_num_splits_from(row, col).len()
 
         let mut count = 0;
 
@@ -47,6 +47,37 @@ impl Manifold {
         count
     }
 
+    pub fn get_num_timelines(&self) -> usize {
+        let (start_row, start_col) = self.start.expect("Start _must_ be set");
+
+        let mut beams = HashSet::<usize>::new();
+        beams.insert(start_col);
+
+        let (_, mut min_col) = *self.splitters.iter().min_by_key(|&(_, col)| col).unwrap();
+        let (_, mut max_col) = *self.splitters.iter().max_by_key(|&(_, col)| col).unwrap();
+
+        // We need to look at values at least one less/greater than the min/max splitter.
+        min_col -= 1;
+        max_col += 1;
+
+        let mut map = HashMap::<usize, usize>::new();
+        let mut values = Vec::<usize>::new();
+        values.resize(max_col + 1, 1);
+
+        // Populate initial states
+        for col in min_col..=max_col {
+            map.insert(col, 1);
+        }
+
+        // Work backwards merging the splitters. Once we hit the start coordinate we are done.
+        for row in (start_row..self.depth).rev() {
+            for (_, col) in self.splitters.iter().filter(|&(r, _)| r == &row) {
+                values[*col] = values[*col - 1] + values[*col + 1];
+            }
+        }
+
+        values[start_col]
+    }
 }
 
 #[cfg(test)]
@@ -80,5 +111,7 @@ mod tests {
         assert!(manifold.splitters.contains(&(8, 6)));
 
         assert_eq!(manifold.get_num_splits(), 21);
+
+        assert_eq!(manifold.get_num_timelines(), 40);
     }
 }
