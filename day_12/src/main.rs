d41fcaf47401e0cfd1f55fc47fc7ad1b25eb57fe
Samuel O'Malley <git@sam.omalley.cc>
2025-12-12 19:25:42 +1030

    You've got to be kidding me. Day 12 part 1/2 solved

--- day_12/src/main.rs
+++ day_12/src/main.rs
@@ -16,14 +16,11 @@ fn run(config: Config) -> Result<(), Box<dyn error::Error>> {
 
     let mut shape = Vec::new();
     let mut shapes = Vec::new();
+    let mut spaces = Vec::new();
+
     for line in contents.lines().map(str::trim) {
         if line.contains("x") {
-            let mut iter = line.split(":");
-            let size = iter.next().unwrap().trim();
-            let requirements = iter.next().unwrap().trim();
-
-            println!("Size: {size}");
-            println!("Requirements: {requirements}");
+            spaces.push(Space::new(line));
         } else if line.is_empty() {
             shapes.push(Shape::new(&shape));
             shape.clear();
@@ -37,6 +34,39 @@ fn run(config: Config) -> Result<(), Box<dyn error::Error>> {
         println!("{shape}");
         println!();
     }
+
+    let mut easy_fit = 0;
+    let mut easy_reject = 0;
+    let mut other = 0;
+    let mut total = 0;
+    for space in spaces {
+        println!(
+            "Space ({}x{}) [{}]:",
+            space.width,
+            space.length,
+            space.area()
+        );
+        //println!("{space}");
+        //println!();
+
+        let (min, max) = space.get_bounds(&shapes);
+        println!("{min} -> {max}");
+
+        if max <= space.area() {
+            easy_fit += 1;
+        } else if min > space.area() {
+            easy_reject += 1;
+        } else {
+            other += 1;
+        }
+        total += 1;
+    }
+
+    println!("Day 12 Part 1");
+    println!(" - Easy Reject: {easy_reject}");
+    println!(" - Easy Fit: {easy_fit}");
+    println!(" - Other: {other}");
+    println!(" - Total: {total}");
 
     Ok(())
 }
