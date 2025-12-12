1189d9e368157fead6b7c86716d4e14fb0d708f8
Samuel O'Malley <git@sam.omalley.cc>
2025-12-12 23:46:32 +1030

    Complete day 2 2015 part 2

--- 2015/day_02/src/lib.rs
+++ 2015/day_02/src/lib.rs
@@ -1,5 +1,21 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+pub fn calculate_paper(s: &str) -> i64 {
+    let mut iter = s.split("x");
+
+    let l: i64 = iter.next().unwrap().parse().unwrap();
+    let w: i64 = iter.next().unwrap().parse().unwrap();
+    let h: i64 = iter.next().unwrap().parse().unwrap();
+
+    (2 * l * w) + (2 * w * h) + (2 * h * l) + (l * w).min(w * h).min(h * l)
+}
+
+pub fn calculate_ribbon(s: &str) -> i64 {
+    let mut iter = s.split("x");
+
+    let l: i64 = iter.next().unwrap().parse().unwrap();
+    let w: i64 = iter.next().unwrap().parse().unwrap();
+    let h: i64 = iter.next().unwrap().parse().unwrap();
+
+    (l + l + w + w).min(w + w + h + h).min(h + h + l + l) + (w * h * l)
 }
 
 #[cfg(test)]
@@ -8,7 +24,10 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        assert_eq!(calculate_paper("2x3x4"), 58);
+        assert_eq!(calculate_paper("1x1x10"), 43);
+
+        assert_eq!(calculate_ribbon("2x3x4"), 34);
+        assert_eq!(calculate_ribbon("1x1x10"), 14);
     }
 }
