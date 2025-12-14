03a7d4e7112c93ad25840382bc59acd136839bd8
Samuel O'Malley <git@sam.omalley.cc>
2025-12-15 01:08:35 +1030

    Migrate 2015 day 5 to new template

--- 2015/src/bin/05.rs
+++ 2015/src/bin/05.rs
@@ -1,11 +1,45 @@
 aoc_2015::solution!(5);
+use fancy_regex::Regex;
 
 pub fn part_one(input: &str) -> Option<u64> {
-    None
+    let mut counter = 0;
+    for line in input.lines() {
+        let line = line.trim();
+
+        if is_nice_part_1(line) {
+            counter += 1;
+        }
+    }
+
+    Some(counter)
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
-    None
+    let mut counter = 0;
+    for line in input.lines() {
+        let line = line.trim();
+
+        if is_nice_part_2(line) {
+            counter += 1;
+        }
+    }
+
+    Some(counter)
+}
+
+pub fn is_nice_part_1(s: &str) -> bool {
+    let repeated_chars = Regex::new(r"([a-z])\1").unwrap().is_match(s).unwrap();
+    let num_vowels = Regex::new(r"[aeiou]").unwrap().find_iter(s).count();
+    let contains_forbidden = Regex::new(r"(ab|cd|pq|xy)").unwrap().is_match(s).unwrap();
+
+    repeated_chars && num_vowels >= 3 && !contains_forbidden
+}
+
+pub fn is_nice_part_2(s: &str) -> bool {
+    let duplicate_pairs = Regex::new(r"([a-z]{2}).*\1").unwrap().is_match(s).unwrap();
+    let triplet = Regex::new(r"([a-z]).\1").unwrap().is_match(s).unwrap();
+
+    duplicate_pairs && triplet
 }
 
 #[cfg(test)]
@@ -13,14 +47,19 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_part_one() {
-        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+    fn part_1() {
+        assert!(is_nice_part_1("ugknbfddgicrmopn"));
+        assert!(is_nice_part_1("aaa"));
+        assert!(!is_nice_part_1("jchzalrnumimnmhp"));
+        assert!(!is_nice_part_1("haegwjzuvuyypxyu"));
+        assert!(!is_nice_part_1("dvszwmarrgswjxmb"));
     }
 
     #[test]
-    fn test_part_two() {
-        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+    fn part_2() {
+        assert!(is_nice_part_2("qjhvhtzxzqqjkmpb"));
+        assert!(is_nice_part_2("xxyxx"));
+        assert!(!is_nice_part_2("uurcxstgmygtbstg"));
+        assert!(!is_nice_part_2("ieodomkazucvgmuy"));
     }
 }
