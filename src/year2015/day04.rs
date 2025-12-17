7d089b3c336b74f788fb97860506e8970c2b4ceb
Samuel O'Malley <git@sam.omalley.cc>
2025-12-17 22:43:02 +1030

    Add rayon parallelisation to md5 hash problem

--- src/year2015/day04.rs
+++ src/year2015/day04.rs
@@ -1,29 +1,40 @@
+use rayon::prelude::*;
+
 pub fn parse(input: &str) -> &str {
-    input
+    input.trim()
 }
 
-pub fn part1(input: &str) -> usize {
+pub fn part1(input: &str) -> u64 {
     find_zero_prefix_md5_hash(input, check_digest_part_1)
 }
 
-pub fn part2(input: &str) -> usize {
+pub fn part2(input: &str) -> u64 {
     find_zero_prefix_md5_hash(input, check_digest_part_2)
 }
 
-fn find_zero_prefix_md5_hash(input: &str, check: fn(&md5::Digest) -> bool) -> usize {
+fn find_zero_prefix_md5_hash(input: &str, check: fn(&md5::Digest) -> bool) -> u64 {
     let mut counter = 0;
+    let batch = 1024 * 1024;
+
     loop {
-        let digest = md5::compute((input.to_string() + &counter.to_string()).as_bytes());
-        if check(&digest) {
+        let result = (counter..counter + batch)
+            .into_par_iter()
+            .map(|i| {
+                (
+                    i,
+                    md5::compute((input.to_string() + &i.to_string()).as_bytes()),
+                )
+            })
+            .find_any(|(_, x)| check(x));
+
+        if let Some(result) = result {
             #[cfg(test)]
-            {
-                eprintln!("{input}{counter}: {:x}", digest);
-            }
-            break;
+            eprintln!("{}: {:?}", result.0, result.1);
+            return result.0;
+        } else {
+            counter += batch;
         }
-        counter += 1;
     }
-    counter
 }
 
 fn check_digest_part_1(digest: &md5::Digest) -> bool {
