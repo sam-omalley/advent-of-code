37a240b3bf254187fe3a28f66825f5d2bcd8a05d
Samuel O'Malley <git@sam.omalley.cc>
2025-12-21 00:43:16 +1030

    Solve 2015 day 12 part 2

--- src/year2015/day12.rs
+++ src/year2015/day12.rs
@@ -2,6 +2,7 @@
 #![allow(clippy::missing_panics_doc)]
 
 use regex::Regex;
+use serde_json::Value;
 
 #[must_use]
 pub fn parse(input: &str) -> &str {
@@ -19,8 +20,36 @@ pub fn part1(input: &str) -> i64 {
 }
 
 #[must_use]
-pub fn part2(_input: &str) -> i64 {
-    0
+pub fn part2(input: &str) -> i64 {
+    let v: Value = serde_json::from_str(input).unwrap();
+    walk(&v)
+}
+
+#[must_use]
+pub fn obj_contains_value(map: &serde_json::Map<String, Value>) -> bool {
+    for val in map.values() {
+        if let Value::String(s) = val
+            && s == "red"
+        {
+            return true;
+        }
+    }
+    false
+}
+
+pub fn walk(value: &Value) -> i64 {
+    match value {
+        Value::Array(arr) => arr.iter().map(walk).sum(),
+        Value::Object(obj) => {
+            if obj_contains_value(obj) {
+                0
+            } else {
+                obj.values().map(walk).sum()
+            }
+        }
+        Value::Number(n) => n.as_i64().unwrap(),
+        Value::Null | Value::Bool(_) | Value::String(_) => 0,
+    }
 }
 
 #[cfg(test)]
@@ -37,5 +66,13 @@ mod tests {
         assert_eq!(part1(r#"[-1,{"a":1}]"#), 0);
         assert_eq!(part1(r#"[]"#), 0);
         assert_eq!(part1(r#"{}"#), 0);
+    }
+
+    #[test]
+    fn part_two() {
+        assert_eq!(part2(r"[1,2,3]"), 6);
+        assert_eq!(part2(r#"[1,{"c":"red","b":2},3]"#), 4);
+        assert_eq!(part2(r#"{"d":"red","e":[1,2,3,4],"f":5}"#), 0);
+        assert_eq!(part2(r#"[1,"red",5]"#), 6);
     }
 }
